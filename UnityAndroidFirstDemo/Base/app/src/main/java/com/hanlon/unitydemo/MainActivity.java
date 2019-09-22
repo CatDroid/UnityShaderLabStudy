c268e20d850205426cc8968fcedd6fd63449bb08
hohanloong <1198432354@qq.com>
2019-09-22 23:00:39 +0800

    Base模块启动 UnityPlayerActivity

---
+++ UnityAndroidFirstDemo/Base/app/src/main/java/com/hanlon/unitydemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.hanlon.unitydemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.hanlon.firstDemo.UnityPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        findViewById(R.id.btnGoUnity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, UnityPlayerActivity.class);
+                MainActivity.this.startActivity(intent);
+                finish();
+
+            }
+        });
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
